8cf78d87b05b2ba19e1882809624da5e25a755ab
mick <hi.mikkel@outlook.com>
2025-06-08 00:16:57 +0200

    fix: so i can use @

--- app.config.ts
+++ app.config.ts
@@ -1,10 +1,19 @@
-import { defineConfig } from '@tanstack/start/config';
 import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from '@tanstack/start/config';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
     vite: {
         plugins: [
             tailwindcss(),
         ],
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './app'),
+            },
+        },
     }
 });
